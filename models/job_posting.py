acb27f9e7afdaeee9bf15e8e7d922bdf76c4b3d0
suk924600 <suk924600@naver.com>
2024-12-08 13:31:02 +0900

    fix: 채용 공고 모델 수정

--- models/job_posting.py
+++ models/job_posting.py
@@ -2,9 +2,13 @@ from services.database import db
 
 class JobPosting:
     @staticmethod
-    def create_job(data):
+    def create(data):
         db.job_postings.insert_one(data)
 
     @staticmethod
-    def get_all_jobs():
-        return list(db.job_postings.find({}, {"_id": 0}))
+    def find_all(filters, skip, limit):
+        return list(db.job_postings.find(filters).skip(skip).limit(limit))
+
+    @staticmethod
+    def find_by_id(job_id):
+        return db.job_postings.find_one({"_id": job_id})
