1217e53160a79ee67e1ee883256aa63565fe3210
suk924600 <suk924600@naver.com>
2024-12-08 13:08:45 +0900

    fix: app.py 로직 수정

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask
+from flasgger import Swagger
 from routes.users import users_bp
 from routes.jobs import jobs_bp
 from routes.applications import applications_bp
+from routes.auth import auth_bp
 
 app = Flask(__name__)
+Swagger(app)
 
-# 라우트 등록
+# 블루프린트 등록
 app.register_blueprint(users_bp, url_prefix="/api/users")
 app.register_blueprint(jobs_bp, url_prefix="/api/jobs")
 app.register_blueprint(applications_bp, url_prefix="/api/applications")
+app.register_blueprint(auth_bp, url_prefix="/api/auth")
 
 # 메인 페이지
 @app.route("/")
