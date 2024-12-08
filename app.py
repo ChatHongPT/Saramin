a27c41f0173cf1c98c906740526bac733e1a3b94
suk924600 <suk924600@naver.com>
2024-12-08 12:55:59 +0900

    fix: app.py 수정

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.users import users_bp
 from routes.jobs import jobs_bp
 from routes.applications import applications_bp
@@ -9,6 +9,11 @@ app = Flask(__name__)
 app.register_blueprint(users_bp, url_prefix="/api/users")
 app.register_blueprint(jobs_bp, url_prefix="/api/jobs")
 app.register_blueprint(applications_bp, url_prefix="/api/applications")
+
+# 메인 페이지
+@app.route("/")
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
