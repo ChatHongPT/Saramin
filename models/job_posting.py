5168c846350fe998dbb4fa85f811a9e43e8b3592
suk924600 <suk924600@naver.com>
2024-12-08 12:58:04 +0900

    fix: 채용 공고 모델 수정

--- models/job_posting.py
+++ models/job_posting.py
@@ -1,8 +1,4 @@
-from pymongo import MongoClient
-from config import MONGO_URI, DB_NAME
-
-client = MongoClient(MONGO_URI)
-db = client[DB_NAME]
+from services.database import db
 
 class JobPosting:
     @staticmethod
@@ -12,11 +8,3 @@ class JobPosting:
     @staticmethod
     def get_all_jobs():
         return list(db.job_postings.find({}, {"_id": 0}))
-
-    @staticmethod
-    def find_job_by_id(job_id):
-        return db.job_postings.find_one({"_id": job_id})
-
-    @staticmethod
-    def delete_job(job_id):
-        db.job_postings.delete_one({"_id": job_id})
